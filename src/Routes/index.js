8f34a6c5cd9159374467e5167b6e9acba6e50126
kelia123 <atetek047@gmail.com>
2022-01-27 10:38:06 +0200

    fourthpush

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Home from "../Views/home";
+import Home from "../Views/Home";
 // import ReactDOM  from 'react-dom';
 import {Routes, Route} from "react-router-dom";
 
