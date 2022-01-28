b0abcd652fcb35d7e239a73ae0103fab74ed15b4
Nicole-Mbera <mberanicole@gmail.com>
2022-01-28 09:30:01 +0200

    shop all

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import Home from "../Views/Home";
 // import ReactDOM  from 'react-dom';
 import {Routes, Route} from "react-router-dom";
+import ShopAll from "../Views/shopAll";
 
 const Index=()=>{
     return(
         <Routes>
             <Route exact path="/home" element={<Home/>}/>
+            <Route exact path="/shopAll" element={<ShopAll/>}/>
         </Routes>
     )
 }
---
+++ src/Views/shopAll.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./shopAll.css";
+import HomeLayout from "../Components/HomeLayout";
+import wallpaper from "../Assets/img/images/crop.jpg"
+import products from "../Assets/constants/shop.json"
+
+
+const ShopAll=()=>{
+
+    return(
+ <div className="shop-container">
+<HomeLayout>
+    <div className="wallpaper"> 
+    <img src={wallpaper}/>
+    </div>
+    <div className="products"> 
+ <h1> Our collections</h1>
+ <div className="bodyScrubs">
+     {/* <h2 className="titles">Body scrubs</h2> */}
+{products.map ((products)=>(
+     <div className="container">
+         <img src={products.picture}/>
+         <h3>{products.name}</h3>
+         <p>{products.description}</p>
+         <p>{products.price}</p>
+     </div>
+))}
+
+ </div>
+    </div>
+
+</HomeLayout>
+
+ </div> 
+    )
+}
+
+export default ShopAll;
