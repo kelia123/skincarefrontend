2626861fc3fd5c16b5944fe4d42d99434c31f57c
triphine <phine.iradukunda18@gmail.com>
2022-01-31 15:47:09 +0200

    contactus

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -3,9 +3,10 @@ import React from "react";
 import Home from "../Views/home";
 import SignUp from"../Views/signup";
 import SignIn from "../Views/signin";
-import Home from "../Views/Home";
+//import Home from "../Views/Home";
 // import ReactDOM  from 'react-dom';
 import {Routes, Route} from "react-router-dom";
+import ContactUs from "../Views/ContactUs ";
 
 
 const Index=()=>{
@@ -14,6 +15,7 @@ const Index=()=>{
             <Route exact path="/home" element={<Home/>}/>
             <Route exact path="/signup" element={<SignUp/>}></Route>
             <Route exact path="/signin" element={<SignIn/>}></Route>
+            <Route exact path="/ContactUs"element={<ContactUs/>}></Route>
         </Routes>
     )
 }
